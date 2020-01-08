55c8987b65fec25f3367053d0da454419eba6125
yusufunlu <unlu.yufus@gmail.com>
2020-01-09 01:10:50 +0300

    Selected sort - klargest alg added

--- src/KLargest.java
+++ src/KLargest.java
@@ -35,16 +35,55 @@ public class KLargest {
         return larges;
     }
 
+    public static void klargest(int[] arr, int k) {
+        int temp = 0;
+        System.out.println("Started:" );
+        System.out.println(Arrays.toString(arr));
+        for(int i = 0; i<k; i++) {
+
+            for(int j = i; j<arr.length-1; j++) {
+                if( arr[j+1]> arr[i]) {
+
+                    temp = arr[i];
+                    arr[i] = arr[j+1];
+                    arr[j+1] = temp;
+                    System.out.println(Arrays.toString(arr));
+                }
+            }
+
+        }
+    }
+
+
+    public static void select2(int[] arr, int k) {
+        int temp = 0;
+        System.out.println("Started:" + Arrays.toString(arr));
+        int num = arr.length;
+        for (int i = 0; i < ( num - 1 ); i++) {
+            for (int j = 0; j < num - i - 1; j++) {
+                if (arr[j] > arr[j+1])
+                {
+                    temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    System.out.println(Arrays.toString(arr));
+                }
+            }
+        }
+
+    }
+
     public static void main(String[] args) {
 
-        Integer arr[] = new Integer[] { 1, 23, 12, 9,
-                30, 2, 50 };
+        Integer arr[] = new Integer[] { 1, 23, 12, 9, 30, 2, 50 };
+        int arr2[] = new int[] { 1, 23, 12, 9, 30, 2, 50 };
+        int arr3[] = new int[] { 1, 23, 12, 9, 30, 2, 50 };
+        int k = 6;
 
-        int arr2[] = new int[] { 1, 23, 12, 9,
-                30, 2, 50 };
-        int k = 3;
-        solution(arr,k);
-        solution2(arr2,k);
+        //solution(arr,k);
+        //solution2(arr2,k);
+        klargest(arr2,k);
+        //select2(arr3,k);
     }
 
 }
